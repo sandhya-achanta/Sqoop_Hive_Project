3f56f6c2f613da0be197c9b70d10cc8dc9442932
sandhya achanta <sandhya achanta>
2017-05-24 19:40:05 -0700

    load Multiple files1

---
+++ SQLScripts/LoadMultipleFiles.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+cd /home/cloudera/user_upload_dump_dir
+for file in $(find . -type f -name "user_upload_dump.*")
+do
+  echo "Processing $file file..."
+  # take action on each file. move each file to user_upload_dump
+  mv $file /home/cloudera/user_upload_dump
+  cd /home/cloudera/
+  #create directory to be process
+  mkdir /home/cloudera/user_upload_dump_WorkInProgress
+  #create directory to stage processed file 
+  mkdir /home/cloudera/user_upload_dump_loadedfiles
+  cd /home/cloudera/user_upload_dump
+  #copy file to uer_upload_dump_WorkInProgress
+  cp  $file  /home/cloudera/user_upload_dump_WorkInProgress
+  cd /home/cloudera
+  #remove directory if exists
+  hadoop dfs -rmr hdfs://quickstart.cloudera:8020/user/cloudera/user_upload_dump_WorkInProgress
+  #copy file Directory to HDFS
+  hadoop fs -put /home/cloudera/user_upload_dump_WorkInProgress
+  hadoop fs -cat  /user/cloudera/user_upload_dump_WorkInProgress/$file
+  hive -e "load data  inpath '/user/cloudera/user_upload_dump_WorkInProgress' into  table user_active_dump.user_upload_dump;"
+  #move prcoessed files to  user_upload_dump_loadedfiles
+  mv /home/cloudera/user_upload_dump_WorkInProgress/$file  /home/cloudera/user_upload_dump_loadedfile
+  #remove file from user_upload_dump_WorkInProgress directory incase it failed to move. 
+  rm -rf /home/cloudera/user_upload_dump_WorkInProgress
+  #remove file from user_upload_dump firectory since it is processed
+  rm -rf  /home/cloudera/user_upload_dump/$file 
+done
+hadoop dfs -rmr hdfs://quickstart.cloudera:8020/user/cloudera/user
+sqoop  import --connect jdbc:mysql://localhost/useractive12  --username root --password-file  /user/cloudera/sqoop_passwordfile/sqoop_password  \
+--table user --hive-import --hive-overwrite\
+--hive-table user_active_dump.user --driver com.mysql.jdbc.Driver
+#execute sqoop job
+sqoop job -exec activelog_load
