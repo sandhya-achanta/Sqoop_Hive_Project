cf7f441b285eda59f4fd4f1827834fd2c98cef5f
sandhya-achanta <sandhya.achanta@clairvoyantsoft.com>
2017-06-09 13:16:07 -0700

    Create User_Total.sql

--- SQLScripts/User_Total.sql
+++ SQLScripts/User_Total.sql
@@ -1,14 +1,10 @@
---create user_total table to load data
-CREATE TABLE IF NOT EXISTS user_total (time_ran timestamp,total_users int,users_added int);
---report query
+  #!/bin/sh
+ #User_report Automation
+hive -d --database user_active_dump
+hive -e "CREATE TABLE IF NOT EXISTS user_active_dump.user_total (time_ran timestamp,total_users int,users_added int);"
+hive -e "use user_active_dump;insert into user_total
 select a.ts,a.total,case when (a.total-b.difflastrun) is null then 0 else (a.total-b.difflastrun) end 
 from (SELECT from_unixtime(unix_timestamp()) as ts,count(*) as total from user) a,
 (select sum(total_users) as difflastrun from user_total 
-where time_ran > from_unixtime(unix_timestamp()))b;
--- report query using insert statement
-insert into user_total
-select a.ts,a.total,case when (a.total-b.difflastrun) is null then 0 else (a.total-b.difflastrun) end 
-from (SELECT from_unixtime(unix_timestamp()) as ts,count(*) as total from user) a,
-(select sum(total_users) as difflastrun from user_total 
-where time_ran > from_unixtime(unix_timestamp()))b;
--- write a shell script to run job when we required( need to check with robert)
+where time_ran > from_unixtime(unix_timestamp()))b;"
+
