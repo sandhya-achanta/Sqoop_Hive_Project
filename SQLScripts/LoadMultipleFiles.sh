c71fba3b9d55198c1e1f6133adeb048c8a93c61e
sandhya-achanta <sandhya.achanta@clairvoyantsoft.com>
2017-06-09 11:24:39 -0700

    Create LoadMultipleFiles.sh

--- SQLScripts/LoadMultipleFiles.sh
+++ SQLScripts/LoadMultipleFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /home/cloudera/user_upload_dump_dir
-for file in $(find . -type f -name "user_upload_dump.*")
+for file in  $(find . -type f -name "user_upload_dump.*")
 do
   echo "Processing $file file..."
   # take action on each file. 
@@ -9,26 +9,26 @@ do
   mkdir /home/cloudera/user_upload_dump_WorkInProgress
   #create directory to stage processed file 
   mkdir /home/cloudera/user_upload_dump_loadedfiles
-  cd /home/cloudera/user_upload_dump
-  #move each file to uer_upload_dump_WorkInProgress
+  cd /home/cloudera/user_upload_dump_dir
+  #move each file to user_upload_dump_WorkInProgress
   mv  $file  /home/cloudera/user_upload_dump_WorkInProgress
-  cd /home/cloudera
+  #remove special character ":" before saving file to hadoop
+  cd /home/cloudera/user_upload_dump_WorkInProgress
+  mv $file ${file//:};
   #remove directory if exists
-  hadoop dfs -rmr hdfs://quickstart.cloudera:8020/user/cloudera/user_upload_dump_WorkInProgress
+  hadoop dfs -rmr /user/cloudera/user_upload_dump_WorkInProgress
   #copy file Directory to HDFS
   hadoop fs -put /home/cloudera/user_upload_dump_WorkInProgress
-  hadoop fs -cat  /user/cloudera/user_upload_dump_WorkInProgress/$file
+  #hadoop fs -cat  /user/cloudera/user_upload_dump_WorkInProgress/$file
   hive -e "load data  inpath '/user/cloudera/user_upload_dump_WorkInProgress' into  table user_active_dump.user_upload_dump;"
   #move prcoessed files to  user_upload_dump_loadedfiles
-  mv /home/cloudera/user_upload_dump_WorkInProgress/$file  /home/cloudera/user_upload_dump_loadedfile
+  mv /home/cloudera/user_upload_dump_WorkInProgress/*  /home/cloudera/user_upload_dump_loadedfile
   #remove file from user_upload_dump_WorkInProgress directory incase it failed to move. 
-  rm -rf /home/cloudera/user_upload_dump_WorkInProgress
-  #remove file from user_upload_dump firectory since it is processed
-  rm -rf  /home/cloudera/user_upload_dump/$file 
+  rm -r /home/cloudera/user_upload_dump_WorkInProgress
 done
-hadoop dfs -rmr hdfs://quickstart.cloudera:8020/user/cloudera/user
-sqoop  import --connect jdbc:mysql://localhost/useractive12  --username root --password-file  /user/cloudera/sqoop_passwordfile/sqoop_password  \
+hadoop dfs -rmr /user/cloudera/user
+sqoop  import --connect jdbc:mysql://localhost/user_active_log --username root --password-file  /user/cloudera/sqoop_passwordfile/password  \
 --table user --hive-import --hive-overwrite\
---hive-table user_active_dump.user --driver com.mysql.jdbc.Driver
+  --hive-table user_active_dump.user --driver com.mysql.jdbc.Driver
 #execute sqoop job
-sqoop job -exec activelog_load
+sqoop job -exec activity_log
